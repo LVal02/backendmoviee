1074a3e1a7f294ba41cfb9985fd13b779cd70b2a
Valentin Li <li.valentin2@gmail.com>
2023-04-19 15:05:33 +0200

    test

--- urls.js
+++ urls.js
@@ -1,7 +1,7 @@
 // Insert the backend URL given by Vercel in this variable
-const BACKEND_URL = '';
+const BACKEND_URL = 'https://backendmoviee-eruf10ith-lval02.vercel.app/';
 
 // Insert the frontend URL given by Vercel in this variable
-const FRONTEND_URL = '';
+const FRONTEND_URL = 'https://frontend-moviepart5-jn56-193qqi6gv-lval02.vercel.app/';
 
 module.exports = { BACKEND_URL, FRONTEND_URL }; // Do not edit/remove this line
