adab433d5cb90292693cbdb8ab4552eaec905e21
LVal02 <128307579+LVal02@users.noreply.github.com>
2023-04-19 15:45:26 +0200

    Update index.js
    nesw

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,11 @@ var router = express.Router();
 
 
 
-router.get('/movies', (req, res) => {
-    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.OWM_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`)
-    .then(response => response.json())
-    .then(movieData => {
-      console.log(movieData);
-      res.json({movies : movieData.results})
+
+router.get('/test, (req, res) => {
+res.json({ result : true})
     })
-})    
+})  
 
 module.exports = router;
 
