1bb0e32660a7adf679af49a7390a591177099571
Valentin Li <li.valentin2@gmail.com>
2023-04-19 14:17:18 +0200

    ze

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 const connectionString = process.env.CONNECTION_STRING;
+const OWM_API_KEY="cb0f21b8dd90556e085481a2ed9bab62"
 
 
 router.get('/movies', (req, res) => {
-    fetch(connectionString)
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${OWM_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`)
     .then(response => response.json())
     .then(movieData => {
       console.log(movieData);
-      res.json({movieData})
+      res.json({movies : movieData.results})
     })
 })    
 // router.get('/config', (req, res) => {
