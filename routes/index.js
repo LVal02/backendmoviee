25b04056d47d26d573804bcfb9b6316eb03d1608
Valentin Li <li.valentin2@gmail.com>
2023-04-19 15:56:12 +0200

    m

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,14 @@ var router = express.Router();
 
 
 
+router.get('/movies', (req, res) => {
+  fetch('https://api.themoviedb.org/3/discover/movie?api_key=cb0f21b8dd90556e085481a2ed9bab62&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate')
+  .then(response => response.json())
+  .then(movieData => {
+    console.log(movieData);
+    res.json({movies : movieData.results})
+  })
+})    
 
-router.get('/test', (req, res) => {
-res.json({ result : true})
-})  
-
-router.get('/test', (req, res) => {
-  res.json({ result : true})
-  })  
 module.exports = router;
 
